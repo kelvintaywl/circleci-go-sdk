85b67c89d333c0c398ec524abe668239a4c3ca4a
kelvintaywl <kelvintaywl@gmail.com>
2023-02-24 23:51:52 +0900

    feat: generate

--- client/circleci_client.go
+++ client/circleci_client.go
@@ -10,7 +10,7 @@ import (
 	httptransport "github.com/go-openapi/runtime/client"
 	"github.com/go-openapi/strfmt"
 
-	"github.com/kelvintaywl/circleci-go-sdk/client/project_envvar"
+	"github.com/kelvintaywl/circleci-go-sdk/client/project"
 	"github.com/kelvintaywl/circleci-go-sdk/client/schedule"
 	"github.com/kelvintaywl/circleci-go-sdk/client/webhook"
 )
@@ -57,7 +57,7 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *Circleci {
 
 	cli := new(Circleci)
 	cli.Transport = transport
-	cli.ProjectEnvvar = project_envvar.New(transport, formats)
+	cli.Project = project.New(transport, formats)
 	cli.Schedule = schedule.New(transport, formats)
 	cli.Webhook = webhook.New(transport, formats)
 	return cli
@@ -104,7 +104,7 @@ func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
 
 // Circleci is a client for circleci
 type Circleci struct {
-	ProjectEnvvar project_envvar.ClientService
+	Project project.ClientService
 
 	Schedule schedule.ClientService
 
@@ -116,7 +116,7 @@ type Circleci struct {
 // SetTransport changes the transport on the client and all its subresources
 func (c *Circleci) SetTransport(transport runtime.ClientTransport) {
 	c.Transport = transport
-	c.ProjectEnvvar.SetTransport(transport)
+	c.Project.SetTransport(transport)
 	c.Schedule.SetTransport(transport)
 	c.Webhook.SetTransport(transport)
 }
